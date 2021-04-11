6b782ebec1bf580a944d601a42981944ed374479
Daniel Jimenez <djimenez@leal.co>
2021-04-10 19:14:45 -0500

    add params in a method

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
     @Get("flavors")
     public findAll(){
         return 'This action returns all coffees';
+    }
+
+    @Get(":id")
+    public findOne(@Param() params){
+        let {id} = params;
+        return `This action returns #${id} coffee`;
     }
 }
