881b2710686b3b0d49e99249a1d96cfe99a1476a
Daniel Jimenez <djimenez@leal.co>
2021-04-11 14:04:30 -0500

    Create DTO from create and update actions verbs

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,6 +1,8 @@
 import { Delete, Patch, Query } from '@nestjs/common';
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -19,13 +21,13 @@ export class CoffeesController {
     }
 
     @Post()
-    public create(@Body() body){
-        return this.coffeesService.create(body);
+    public create(@Body() createCoffeeDto:CreateCoffeeDto){
+        return this.coffeesService.create(createCoffeeDto);
     }
     
     @Patch(':id')
-    public update(@Param('id')id:string, @Body() body){
-        return this.coffeesService.update(id, body);
+    public update(@Param('id')id:string, @Body() updateCoffeeDto:UpdateCoffeeDto){
+        return this.coffeesService.update(id, updateCoffeeDto);
     }
     
     @Delete(':id')
