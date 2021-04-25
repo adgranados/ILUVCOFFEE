374c8c93fc12e3657688a8c703bfae753e3330ff
Daniel Jimenez <djimenez@leal.co>
2021-04-25 17:45:38 -0500

    CREATING_CUSTOM_PIPES

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -2,6 +2,7 @@ import { Delete, Patch, Query } from '@nestjs/common';
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { Public } from 'src/common/decorators/public.decorator';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
+import { ParseIntPipe } from 'src/common/pipes/parse-int.pipe';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
@@ -20,7 +21,8 @@ export class CoffeesController {
     }
 
     @Get(":id")
-    public findOne(@Param('id') id:number){
+    public findOne(
+        @Param('id', ParseIntPipe) id:number){
         return this.coffeesService.findOne(id);
     }
 
