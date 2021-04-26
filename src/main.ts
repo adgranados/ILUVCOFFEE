4776192c7575888108a994ff7ce1313d0f7176d9
Daniel Jimenez <djimenez@leal.co>
2021-04-25 19:44:02 -0500

    INTRODUCING_SWAGGER_MODULE

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
+import { DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -22,6 +23,16 @@ async function bootstrap() {
     new WrapResponseInterceptor(),
     new TimeoutInterceptor()
     );
+
+    const options = new DocumentBuilder()
+    .setTitle('Iluvcoffee')
+    .setDescription("Coffe Applicaction")
+    .setVersion('1.0')
+    .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.HTTP_PORT || 3000);
 }
 bootstrap();
